6ce1e66d81b600a6fb2bafbc98a550e91718b7c0
lucas-cell <lucasnieddu@gmail.com>
2021-12-30 17:18:14 -0700

    Update Repo

--- coupon_collector.py
+++ coupon_collector.py
@@ -5,62 +5,96 @@ Description
     problem 
 
 """
-import random as rd
+import random
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sbn
 
-class CollectCoupons:
+coupons = []
+uniques = []
+winners = []
+purchases = []
 
-    def __init__(self, num_uniques):
-        self.num_uniques = num_uniques
-        self.coupons_list = []
-        self.uniques_list = []
-        self.winning_list = []
-        self.purchase_list = []
+# Generate the winning set of coupons
+def generate_winning_set(num_uniques):
+    for coupon in range(num_uniques):
+        winners.append(coupon + 1)
+    return winners
 
-    # Generate the winning list of coupons needed to win the prize. 
-    def generate_winning_set(self):
-        print("Total number of unique coupons needed to collect: ", str(self.num_uniques))
-        self.winning_list = range(1, self.num_uniques + 1)
-        return self.winning_list
+# Buy an ice cream and collect the corresponding coupon
+def buy_ice_cream(num_uniques):
+    coupon = random.randint(1, num_uniques)
+    coupons.append(coupon)
+    return coupons
 
-    # Buy an ice cream and collect the corresponding coupon
-    def buy_ice_cream(self):
-        coupon = rd.randint(1, self.num_uniques)
-        self.coupons_list.append(coupon)
-        return self.coupons_list
+# Find the unique coupons collected 
+def check_coupons(coupons):
+    for coupon in coupons:
+        if coupon not in uniques:
+            uniques.append(coupon)
+    return uniques
 
-    # Fiter repeating coupons
-    def check_coupons(self):
-        for coupon in self.coupons_list:
-            if coupon not in self.uniques_list:
-                self.uniques_list.append(coupon)
-        return self.uniques_list
-
-    # Buy icecreams until all coupons are collected
-    def play(self):
-        # Used as a counter for the total purchases to win the game. 
-        total_purchases = 0
+# Function to clear lists to start a new game
+def clear_lists():
+    coupons.clear()
+    uniques.clear()
+    winners.clear()
     
-        # Generate winning tickets
-        self.generate_winning_set()
+# Play the coupon collector game 
+def play_game(num_uniques):
+    # Every game is considered a 'new game' so we need to clear the lists
+    clear_lists()
     
-        # Buy ice creams until the set of winning tickets is collected. 
-        while self.uniques_list != self.winning_list:
-            # collect a coupon
-            self.buy_ice_cream()
+    # Used as a counter for the total purchases to win the game. 
+    total_purchases = 0
+    
+    # Generate winning tickets
+    winners = generate_winning_set(num_uniques)
+    
+    # Buy ice creams until the set of winning tickets is collected. 
+    while uniques != winners:
+        # collect a coupon
+        buy_ice_cream(num_uniques)
         
-            # Increase counter by 1
-            total_purchases += 1
+        # Increase counter by 1
+        total_purchases += 1
         
-            # filter the coupons
-            self.check_coupons()
+        # filter the coupons
+        check_coupons(coupons)
         
-            # sort the coupons
-            self.uniques_list.sort()
+        # sort the coupons
+        uniques.sort()
         
-            # If the uniques equals the winning set, the costumer won the prize
-            if(self.uniques_list == self.winning_list):
-                self.purchase_list.append(total_purchases)
-                break
-CC = CollectCoupons(10)
-CC.play()
+        # If the uniques equals the winning set, the costumer won the prize
+        if(uniques == winners):
+            purchases.append(total_purchases)
+            break
+
+def gather_game_data(num_unique, trial_amount):
+    purchases.clear()
+    trials = [num_unique] * trial_amount
+    for trial in trials:
+        play_game(trial)
+    purchases.sort()
+    minimum = purchases[0]
+    maximum = purchases[trial_amount - 1]
+    mean = sum(purchases) / len(purchases)  
+    var = sum([((x - mean) ** 2) for x in purchases]) / len(purchases)
+    res = var ** 0.5
+    game_data = [num_unique, trial_amount, mean, res, minimum, maximum]
+    return game_data
+
+def gather_all_games_data(coupons_needed_list, trial_amount):
+    num_trials = trial_amount
+    games = []
+    dataframe = pd.DataFrame(columns = ['Coupons Needed', 'Trials', 'Mean', 'Standard Deviation', 'Minimum', 'Maximum'])
+    for i in range(len(coupons_needed_list)):
+        game_data = gather_game_data(num_unique = coupons_needed_list[i], trial_amount = num_trials)
+        games.append(game_data)
+        dataframe.loc[len(dataframe)] = game_data
+    return dataframe
+
+def show_regplot(df, x, y):
+    plt.title("Coupons Purchased")
+    reg_plot = sbn.regplot(x=x, y=y, data=df)
+    return reg_plot
